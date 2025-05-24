d7b740ba5c8973dd59ae0392dd6f25d108c532e0
eoto0 <eoto0@chicagobooth.edu>
2025-05-24 07:48:57 +0000

    edit user added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
    before_action :authenticate_user!
-  skip_forgery_protection
+   
+   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    # for  /users (sign-up)
+    devise_parameter_sanitizer.permit(
+      :sign_up,
+      keys: [:username, :private]
+    )
+
+    # for  /users (PUT/PATCH update)
+    devise_parameter_sanitizer.permit(
+      :account_update,
+      keys: [:username, :private]
+    )
+  end
 end
